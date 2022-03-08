2448d3b8c3ce153a2e99b57328d46a72918aa050
Georgii <galechyan1991@gmail.com>
2022-03-08 22:08:34 +0300

    И-за того что всю неделю я в коммандировке и почти нет времени на задание, перекроил всё!!! Выкинул всю контентную часть, оставил от шапки только каркас. Пол часа на доработку и всё

--- test/src/JS/script.js
+++ test/src/JS/script.js
@@ -2,6 +2,13 @@
 
 $(document).ready(function () {
   $('.header__burger').click(function (event) {
+    // добавляем класс active бургеру при открытом бургерном меню.
+    // стобы потом можно было модифицировать активный бургер
     $('.header__burger,.header__menu').toggleClass('active')
+
+    // добавляем класс lock элементу body при открытом бургерном меню.
+    // потом в css будем ставить запрет на прокрутку при открытом бургерном меню.
+    // запрет на прокрутку - overflow:hidden
+    $('body').toggleClass('lock')
   })
 })
